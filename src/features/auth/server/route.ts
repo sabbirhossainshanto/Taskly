0ad72d7958545d6129b8c4bcbe9e0c18c9eba1e6
sabbirHossain <sabbirshnt@gmail.com>
2025-09-01 12:47:07 +0600

    02-authenticate-hono-appwrite

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,17 +1,43 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../constant";
 
 const app = new Hono()
-  .post("/login", zValidator("json", loginSchema), (c) => {
+  .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
-    console.log({ email, password });
-    return c.json({ email, password });
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+    return c.json({ success: true });
   })
-  .post("/register", zValidator("json", registerSchema), (c) => {
+  .post("/register", zValidator("json", registerSchema), async (c) => {
     const { email, password, name } = c.req.valid("json");
-    console.log({ email, password, name });
-    return c.json({ email, password, name });
+    const { account } = await createAdminClient();
+    await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+    return c.json({ success: true });
+  })
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
